5507fc485b5dfeb65bbfb181a2e0b82e18361500
juntaki <me@juntaki.com>
2020-08-16 16:11:00 +0900

    Fix

--- cmd/caption2/main.go
+++ cmd/caption2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gordonklaus/portaudio"
 	"github.com/gotk3/gotk3/gdk"
+	"github.com/gotk3/gotk3/glib"
 	"github.com/gotk3/gotk3/gtk"
 	"github.com/juntaki/amivoice-go"
 	"github.com/juntaki/amivoice-go/cmd/lib"
@@ -12,7 +13,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"os/signal"
 	"time"
 )
 
@@ -36,7 +36,7 @@ func NewCaption() *Caption {
 		bg[i].SetXAlign(0)
 		bg[i].SetYAlign(0)
 	}
-	line := 3
+	line := 2
 	m := [][]int{
 		{-1, 1},
 		{1, 1},
@@ -55,7 +55,7 @@ func NewCaption() *Caption {
 }
 
 func (c *Caption) setMessage(input string) {
-	splitlen := 100
+	splitlen := 50
 	runes := []rune(input)
 	lastLineLen := len(runes) % splitlen
 	if lastLineLen == 0 {
@@ -73,115 +73,127 @@ func (c *Caption) setMessage(input string) {
 	c.labels[0].SetMarkup(fmt.Sprintf(format, "white", message))
 	bg := fmt.Sprintf(format, "black", message)
 	for _, b := range c.labels[1:] {
-		b.SetMarkup(bg)
+		_, err := glib.IdleAdd(b.SetMarkup, bg)
+		if err != nil {
+			log.Fatal("IdleAdd() failed:", err)
+		}
 	}
 }
 
 func main() {
-	setting, err := lib.ReadSetting()
+	const appID = "org.gtk.example"
+	application, err := gtk.ApplicationNew(appID, glib.APPLICATION_FLAGS_NONE)
+	// Check to make sure no errors when creating Gtk Application
 	if err != nil {
-		panic(err)
+		log.Fatal("Could not create application.", err)
 	}
 
-	sig := make(chan os.Signal, 1)
-	signal.Notify(sig, os.Interrupt, os.Kill)
+	application.Connect("activate", func() {
+		setting, err := lib.ReadSetting()
+		if err != nil {
+			panic(err)
+		}
 
-	pr, pw := io.Pipe()
+		pr, pw := io.Pipe()
 
-	// PortAudio input with buffer
-	portaudio.Initialize()
-	defer portaudio.Terminate()
-	in := make([]int16, 64)
-	stream, err := portaudio.OpenDefaultStream(1, 0, 16000, len(in), in)
-	if err != nil {
-		panic(err)
-	}
-	defer stream.Close()
-	stream.Start()
-	go func() {
-		for {
+		// PortAudio input with buffer
+		portaudio.Initialize()
+		defer portaudio.Terminate()
+		in := make([]int16, 64)
+		stream, err := portaudio.OpenDefaultStream(1, 0, 16000, len(in), in)
+		if err != nil {
+			panic(err)
+		}
+		defer stream.Close()
+		stream.Start()
+		go func() {
 			for {
-				f, err := stream.AvailableToRead()
+				for {
+					f, err := stream.AvailableToRead()
+					if err != nil {
+						pw.CloseWithError(err)
+					} else if f > 64 {
+						break
+					}
+				}
+				stream.Read()
+				err = binary.Write(pw, binary.LittleEndian, in)
 				if err != nil {
 					pw.CloseWithError(err)
-				} else if f > 64 {
-					break
 				}
 			}
-			stream.Read()
-			err = binary.Write(pw, binary.LittleEndian, in)
-			if err != nil {
-				pw.CloseWithError(err)
-			}
-			select {
-			case <-sig:
-				pw.Close()
-				break
-			default:
-			}
-		}
-	}()
+		}()
 
-	// Initialize amivoice
-	c, err := amivoice.NewConnection(setting.AppKey, setting.NoLog)
-	if err != nil {
-		return
-	}
-	defer c.Close()
-
-	final := make(chan *amivoice.AEvent)
-	progress := make(chan *amivoice.UEvent)
-
-	go c.CollectResult(final, progress, nil)
-	go c.Recognize(setting.GenerateRecognitionConfig(pr))
-
-	gtk.Init(nil)
-
-	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
-	}
-	win.SetPosition(gtk.WIN_POS_CENTER)
-	win.SetTitle("Transcribe")
-	win.Connect("destroy", func() {
-		gtk.MainQuit()
-	})
-	win.Connect("button-press-event", func(w *gtk.Window, e *gdk.Event) {
-		ev := gdk.EventButton{Event: e}
-		if ev.Button() != 1 {
+		// Initialize amivoice
+		c, err := amivoice.NewConnection(setting.AppKey, setting.NoLog)
+		if err != nil {
 			return
 		}
-		win.BeginMoveDrag(int(ev.Button()), int(ev.XRoot()), int(ev.YRoot()), uint32(time.Now().Unix()))
-	})
-	win.SetDecorated(false)
-	win.SetKeepAbove(true)
-	win.SetResizable(true)
-	win.SetAppPaintable(true)
+		defer c.Close()
 
-	finalText := ">"
-	currentText := ">"
+		final := make(chan *amivoice.AEvent)
+		progress := make(chan *amivoice.UEvent)
 
-	cap := NewCaption()
-	tick := time.NewTicker(300 * time.Millisecond)
-	go func() {
-		for {
-			select {
-			case val := <-final:
-				finalText += val.Text
-				currentText = finalText
-			case val := <-progress:
-				currentText = finalText + val.Text
-			case <-tick.C:
-				cap.setMessage(currentText)
-			}
+		go c.CollectResult(final, progress, nil)
+		go c.Recognize(setting.GenerateRecognitionConfig(pr))
+
+		// Create ApplicationWindow
+		win, err := gtk.ApplicationWindowNew(application)
+		if err != nil {
+			log.Fatal("Could not create application window.", err)
 		}
-	}()
+		win.SetPosition(gtk.WIN_POS_CENTER)
+		win.SetTitle("Transcribe")
+		win.Connect("destroy", func() {
+			gtk.MainQuit()
+		})
+		win.Connect("button-press-event", func(w *gtk.ApplicationWindow, e *gdk.Event) {
+			ev := gdk.EventButton{Event: e}
+			if ev.Button() != 1 {
+				return
+			}
+			win.BeginMoveDrag(int(ev.Button()), int(ev.XRoot()), int(ev.YRoot()), uint32(time.Now().Unix()))
+		})
+		win.Connect("window_state_event", func() {
+			win.SetKeepAbove(true)
+		})
+		win.SetDecorated(false)
+		win.SetKeepAbove(true)
+		win.SetAppPaintable(true)
 
-	win.Add(cap.widget)
-	sc := win.GetScreen()
-	v, err := sc.GetRGBAVisual()
-	win.SetVisual(v)
-	win.SetDefaultSize(0, 0)
-	win.ShowAll()
-	gtk.Main()
+		lastText := ""
+		finalText := ">"
+		currentText := ">"
+
+		cap := NewCaption()
+		tick := time.NewTicker(500 * time.Millisecond)
+		go func() {
+			for {
+				select {
+				case val := <-final:
+					finalText += val.Text
+					currentText = finalText
+				case val := <-progress:
+					currentText = finalText + val.Text
+				case <-tick.C:
+					if lastText != currentText {
+						cap.setMessage(currentText)
+						lastText = currentText
+					}
+				}
+			}
+		}()
+
+		win.Add(cap.widget)
+		sc := win.GetScreen()
+		v, err := sc.GetRGBAVisual()
+		win.SetVisual(v)
+		win.SetDefaultSize(10, 10)
+		win.ShowAll()
+		gtk.Main()
+	})
+
+	// Run Gtk application
+	application.Run(os.Args)
+
 }
